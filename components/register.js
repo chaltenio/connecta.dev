47b220f3281e72ae9289244acff9df3968c06c21
chaltenio <dev@chalten.io>
2022-04-29 00:08:31 +1200

    Added register funcionality

---
+++ components/register.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
+import styles from '../styles/Home.module.css'
+
+export default function newsletter() {
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("IDLE");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const subscribe = async () => {
+    setState("LOADING");
+    setErrorMessage(null);
+    try {
+      const response = await axios.post("/api/register", { email });
+      setState("SUCCESS");
+    } catch (e) {
+      setErrorMessage(e.response.data.error);
+      setState("ERROR");
+    }
+  };
+
+  return (
+    <div>
+      <h3 className={styles.subtitle}>
+        Connect / Share / Learn
+      </h3>
+
+      <p className={styles.descriptiontitle}>
+        Please register your interest...
+      </p>      
+
+      <div className={styles.registerMailchimp}>
+        <input className={styles.registerMailchimpInput}         
+          type="text"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="button" className={styles.registerMailchimpButton} disabled={state === "LOADING"} onClick={subscribe}>Register</button>
+      </div>
+
+      {state === "ERROR" && (
+        <p className={styles.description}>{errorMessage}</p>
+      )}
+
+      {state === "SUCCESS" && (
+        <p className={styles.description}>Thanks! We will be in touch shortly :)</p>
+      )}
+
+    </div>
+  );
+};
+
+
+// Attributions:
+// https://github.com/uriklar/uriklar-next/blob/master/components/newsletter.jsx
