83b71e6a7d218404bd3f4d810591dfb9ca76fd26
chaltenio <dev@chalten.io>
2022-04-29 00:27:46 +1200

    Adding register reference

--- pages/githubactions.js
+++ pages/githubactions.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Newsletter from "../components/newsletter";
+import Newsletter from "../components/register";
 import styles from '../styles/Home.module.css'
 import Register from './api/register';
 
