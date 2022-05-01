324b04b2c89ea6fedb66e013ceeaa1b5018ba52f
chaltenio <dev@chalten.io>
2022-05-01 19:55:25 +1200

    File rename

--- pages/githubdevops.js
+++ pages/githubdevops.js
@@ -15,6 +15,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h2>Thursday May 12th, 2022 - 5:30pm (NZT) - ONLINE</h2>
+
         <h1 className={styles.title}>
         End-to-end DevOps in the Cloud with GitHub
         </h1>
@@ -31,19 +33,27 @@ export default function Home() {
 
       <footer className={styles.footer}>      
         <a
-          href="https://twitter.com/matedotdev"
+          href="https://twitter.com/connectadev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          @matedotdev
+          @connectadev
         </a>
         {' '} - {' '}
+        <a
+          href="https://discord.com/invite/xT9cMSy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Our Discord Channel. Join us!
+        </a>
+        {' '} - {' '}        
         <a
           href="https://mate.dev/chris"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Contact
+         brought to you by
         </a>
       </footer>
     </div>
